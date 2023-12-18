1bf828cd3b47a019ea112e7a80a4fbba25487c4a
Euphel <jlcsievon@gmail.com>
2023-12-18 23:29:25 +0800

    fixing for deployment

--- actions/fetch-user-details-browser.ts
+++ actions/fetch-user-details-browser.ts
@@ -2,16 +2,6 @@ import { User } from "@/lib/collection.types";
 import { createClient } from "@/utils/supabase/client";
 import { fetchAuthUser } from "./fetch-auth-user";
 
-const templateUser: User = {
-  birth_date: "",
-  first_name: "",
-  gender: "",
-  id: "",
-  last_name: "",
-  middle_name: "",
-  updated_at: "",
-};
-
 export const fetchUserDetails = (userId: string) => {
   const supabase = createClient();
 
