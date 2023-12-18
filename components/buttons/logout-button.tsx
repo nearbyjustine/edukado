761a93ef3c481155eaeeabce81c87a774c9d1f60
Euphel <jlcsievon@gmail.com>
2023-12-19 06:40:38 +0800

    aaa

--- components/buttons/logout-button.tsx
+++ components/buttons/logout-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { logOut } from "@/actions/logout";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 
