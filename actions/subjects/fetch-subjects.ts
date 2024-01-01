5814630274859dd33a044e25280589bd1416693f
Euphel <jlcsievon@gmail.com>
2024-01-02 01:56:30 +0800

    0

---
+++ actions/subjects/fetch-subjects.ts
@@ -0,0 +1,30 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export const fetchSubject = async () => {
+  const cookieStore = cookies();
+  const supabase = await createClient(cookieStore);
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (!user || error) {
+    return { user: null, error };
+  }
+
+  const { data: classroomData, error: classroomError } = await supabase
+    .from("classrooms")
+    .select(
+      `
+    id,
+    grade_level,
+    section,
+    subjects (id, name)`
+    )
+    .eq("subjects.teacher_id", user.id)
+    .order("grade_level");
+
+  return { classroomData, classroomError };
+};
